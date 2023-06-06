8394e64952d9f3f533d519486da20f0e46f700d9
Andrey_BespaloFF <andrey2802@mail.ru>
2023-06-06 16:27:56 +0300

    quick example

---
+++ my.sql
@@ -0,0 +1,22 @@
+
+-- create
+CREATE TABLE EMPLOYEE (
+  empId INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  age TEXT NOT NULL
+  addres TEXT NOT NULL
+  
+ 
+);
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Clark', '25','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0002, 'Dave',  '26','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0003, 'Ava',   '27','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0004, 'Kirk',  '28','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0005, 'Lars',  '29','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0006, 'James', '23','Washington D.C.');
+INSERT INTO EMPLOYEE VALUES (0007, 'David', '52','Washington D.C.');
+
+
+
